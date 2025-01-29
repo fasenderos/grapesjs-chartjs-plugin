39f08e0702d61851c28778709bafbf98312153d7
Andrea Fassina <fasenderos@gmail.com>
2025-01-29 08:37:24 +0100

    refactor: move loadBlock

--- src/loadBlocks.ts
+++ src/loadBlocks.ts
@@ -1,13 +1,28 @@
 import type { Editor } from "grapesjs";
-import buildBlocks from "./blocks/build-block";
-import { CHARTS } from "./constants";
+import { CHART_TYPE, CHARTS } from "./constants";
 import icons from "./icons";
 
 export default async (editor: Editor) => {
   const bm = editor.BlockManager;
   for (const chart of CHARTS) {
-    const blockType = `chartjs-${chart.type}`;
+    const type = chart.type ?? "bar";
+    const blockType = `chartjs-${type}`;
     const icon = icons[chart.type];
-    bm.add(blockType, buildBlocks(icon, chart));
+    const label =
+      chart.blockLabel ??
+      `${type.charAt(0).toUpperCase() + type.slice(1)} Chart`;
+    bm.add(blockType, {
+      label,
+      category: "Charts",
+      content: {
+        tagName: "div",
+        type: "chartjs",
+        attributes: {
+          "data-gjs-type": "chartjs",
+          [CHART_TYPE]: type,
+        },
+      },
+      media: icon,
+    });
   }
 };
