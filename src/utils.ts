f06373db0a684d88b7d4478ad3e90a618dfb9949
Andrea Fassina <fasenderos@gmail.com>
2025-01-29 14:47:58 +0100

    style: remove i18n placeholder

--- src/utils.ts
+++ src/utils.ts
@@ -2,5 +2,5 @@ import type { Editor } from "grapesjs";
 import { PLUGIN } from "./constants";
 
 export const getI18nName = (editor: Editor, key: string): string => {
-  return `@@@${editor.I18n.t(`${PLUGIN}.${key}`)}`;
+  return editor.I18n.t(`${PLUGIN}.${key}`);
 };
